fdc107d8b851b8dec591590fb2c17d5b4c622147
Jack <29169102+Jack5079@users.noreply.github.com>
2020-04-08 21:47:41 -0400

    remove logs

--- ext.js
+++ ext.js
@@ -1,13 +1,11 @@
 /* global window, document */
 function waitForElementToDisplay () {
   if (window.wrappedJSObject.dogeminer !== undefined && window.wrappedJSObject.dogeminer.news !== undefined) {
-    console.log('donee!')
     const script = document.createElement('script')
     script.src = 'https://cdn.jsdelivr.net/gh/Jack5079/dogeminer-2-cheats/cheats.js'
     document.body.appendChild(script)
     return
   } else {
-    console.log('i be wating still')
     setTimeout(function () {
       waitForElementToDisplay()
     }, 100)
